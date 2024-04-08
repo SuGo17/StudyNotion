8d07dc4115da546d527bf7598b86b230a7cbde9c
Continentalcode <continentalcode01@gmail.com>
2024-04-08 23:17:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-dark">
+        <h1 className="bg-gray-800 text-red-500"> Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
